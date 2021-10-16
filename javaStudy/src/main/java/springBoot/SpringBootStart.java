1eb06e7d5906c11efc0ce3412a4ae2afb10d4b98
luohao <19970303luohao>
2021-10-16 16:50:43 +0800

    spring boot

---
+++ javaStudy/src/main/java/springBoot/SpringBootStart.java
@@ -0,0 +1,22 @@
+package springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication(exclude={XADataSourceAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = {"springBoot"})
+public class SpringBootStart {
+    public static void main(String[] srg){
+        Logger log=Logger.getLogger("main-启动");
+        log.info("spring boot 开始启动");
+        ApplicationContext context= SpringApplication.run(SpringBootStart.class,srg);
+        log.info("spring boot 启动完毕");
+
+    }
+}
