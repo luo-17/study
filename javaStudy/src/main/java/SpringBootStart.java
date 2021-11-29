5905ea3fcb0c964943424c9dc7f8ba02c8e37b2a
luohao <19970303luohao>
2021-11-29 12:02:45 +0800

    spring boot

--- javaStudy/src/main/java/SpringBootStart.java
+++ javaStudy/src/main/java/SpringBootStart.java
@@ -1,5 +1,3 @@
-package springBoot;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
